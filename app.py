0fe6cb9f7581f56bd8d4a7cad430e3ee08d14a26
csk190 <csk190@gmail.com>
2026-04-22 20:33:49 +0900

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import streamlit as st
 from gtts import gTTS
 import io
 
-st.subheader("Text-to-Speech Converter (using Google TTS)")
+st.subheader(":grinning:우리반 원어민 선생님 (using Google TTS)")
 
 text_input = st.text_area("Enter the text you want to convert to speech:")
 
